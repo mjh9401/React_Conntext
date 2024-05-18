73a102532b298e3d582186c0fe946ec04b3d9d49
mjh9401@naver.com <mjh9401@naver.com>
2024-05-18 16:37:25 +0900

    주문 완료 페이지 완성

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import OrderPage from './pages/orderPage';
 import SummaryPage from './pages/summaryPage';
+import CompletePage from './pages/completePage'
 
 function App() {
+  const [step, setStep] = useState(0);
+
   return (
-    <div style={{padding:'4rem'}}>
-      <OrderPage/>
-      <SummaryPage/>
+    <div style={{pafirst:'4rem'}}>
+      {step === 0 && <OrderPage setStep={setStep}/>}
+      {step === 1 && <SummaryPage setStep={setStep}/>}
+      {step === 2 && <CompletePage setStep={setStep}/>}
     </div>
   );
 }
